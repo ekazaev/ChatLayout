31430aa8cebe96604ca8bcb92ded098add1b7b88
Eugene Kazaev <eugene.kazaev@gmail.com>
2024-01-29 21:11:46 +0000

    Litte spacing test

--- Example/ChatLayout/Chat/View/Data Source/DefaultChatCollectionDataSource.swift
+++ Example/ChatLayout/Chat/View/Data Source/DefaultChatCollectionDataSource.swift
@@ -467,6 +467,9 @@ extension DefaultChatCollectionDataSource: RecyclerViewDataSource {
         } else {
             payload.isPinned = false
         }
+        if case .messageGroup = sections.first?.cells[index] {
+            payload.spacing.trailing = 0
+        }
         return payload
     }
 
