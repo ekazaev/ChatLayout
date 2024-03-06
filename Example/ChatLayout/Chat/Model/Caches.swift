1d971ecce47d24336590ff72a0ce279f356ece85
Eugene Kazaev <eugene.kazaev@gmail.com>
2024-03-06 12:10:57 +0000

    Updated caches in the example app

--- Example/ChatLayout/Chat/Model/Caches.swift
+++ Example/ChatLayout/Chat/Model/Caches.swift
@@ -14,15 +14,15 @@ import Foundation
 
 let loader = CachingImageLoader(cache: imageCache, loader: DefaultImageLoader())
 
-// @available(iOS 13, *)
-// var metadataCache = IterativeCache(mainCache: MetaDataCache(cache: MemoryDataCache<URL>()),
-//                                   backupCache: MetaDataCache(cache: PersistentDataCache<URL>(cacheFileExtension: "metadataCache")))
-//
-// let imageCache = IterativeCache(mainCache: ImageForUrlCache(cache: MemoryDataCache<CacheableImageKey>()),
-//                                backupCache: ImageForUrlCache(cache: PersistentDataCache<CacheableImageKey>()))
-
-// Uncomment to reload dynamic content on every start.
 @available(iOS 13, *)
-var metadataCache = MetaDataCache(cache: MemoryDataCache<URL>())
+var metadataCache = IterativeCache(mainCache: MetaDataCache(cache: MemoryDataCache<URL>()),
+                               backupCache: MetaDataCache(cache: PersistentDataCache<URL>(cacheFileExtension: "metadataCache")))
 
-let imageCache = ImageForUrlCache(cache: MemoryDataCache<CacheableImageKey>())
+let imageCache = IterativeCache(mainCache: ImageForUrlCache(cache: MemoryDataCache<CacheableImageKey>()),
+                            backupCache: ImageForUrlCache(cache: PersistentDataCache<CacheableImageKey>()))
+
+//// Uncomment to reload dynamic content on every start.
+//@available(iOS 13, *)
+//var metadataCache = MetaDataCache(cache: MemoryDataCache<URL>())
+//
+//let imageCache = ImageForUrlCache(cache: MemoryDataCache<CacheableImageKey>())
