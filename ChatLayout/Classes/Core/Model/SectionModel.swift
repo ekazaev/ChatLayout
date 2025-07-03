5f95bbf0c4ada7a1e84f59bbecd917d803aef275
Frizlab <fload@me.com>
2025-07-03 23:54:04 +0200

    Remove redundant Sendable declaration on SectionModel

--- ChatLayout/Classes/Core/Model/SectionModel.swift
+++ ChatLayout/Classes/Core/Model/SectionModel.swift
@@ -14,7 +14,7 @@ import Foundation
 import UIKit
 
 @MainActor
-struct SectionModel<Layout: ChatLayoutRepresentation>: Sendable {
+struct SectionModel<Layout: ChatLayoutRepresentation> {
     let id: UUID
 
     let interSectionSpacing: CGFloat
